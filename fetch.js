ba47bf61a1da5e8ef1c336ab867cb8feae59f2e0
Ayala <53246687+scqq6@users.noreply.github.com>
2025-11-21 14:26:57 +0100

    Add fetch functionality to handle API requests

---
+++ fetch.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("button");
+    const output = document.querySelector(".content");
+
+    button.addEventListener("click", async () => {
+        const value = document.getElementById("myvalue").value;
+
+        // Your Lambda URL
+        const apiUrl = "https://rrrorg9iri.execute-api.eu-west-1.amazonaws.com/default/aya-function?name=" + encodeURIComponent(value);
+
+        try {
+            const response = await fetch(apiUrl);
+            const text = await response.text();
+
+            output.textContent = "Response: " + text;
+        } catch (err) {
+            output.textContent = "Error: " + err;
+        }
+    });
+});
