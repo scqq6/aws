43005864ca6a0d49db3102e4e6ed7019061d9299
Ayala <53246687+scqq6@users.noreply.github.com>
2025-11-21 15:01:18 +0100

    Update fetch URL to use live API endpoint

--- fetch.js
+++ fetch.js
@@ -2,7 +2,7 @@ ndow.onload = function() {
  document.getElementById('button').addEventListener('click', function () {
  let val = document.querySelector('input[id="myvalue"]').value;
  console.log(val);
- fetch('https://<<YOUR URL>>?key1='+val)
+ fetch('https://rrrorg9iri.execute-api.eu-west-1.amazonaws.com/default/aya-function?key1='+val)
  .then((response) => {
  return response.text();
  })
