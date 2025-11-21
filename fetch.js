a5d7dd4e58735202f444b172a9bae19186e0da73
Ayala <53246687+scqq6@users.noreply.github.com>
2025-11-21 15:05:56 +0100

    Refactor fetch.js for better event handling
    Refactor fetch.js to improve event handling and URL construction.

--- fetch.js
+++ fetch.js
@@ -1,15 +1,12 @@
-window.onload = function() {
- document.getElementById('button').addEventListener('click', function () {
- let val = document.querySelector('input[id="myvalue"]').value;
- console.log(val);
- fetch('https://rrrorg9iri.execute-api.eu-west-1.amazonaws.com/default/aya-function?key1='+val)
- .then((response) => {
- return response.text();
- })
- .then((myContent) => {
+window.onload = function () {
+    document.getElementById('button').addEventListener('click', function () {
 
-document.querySelector('.content').innerHTML = myContent;
- });
+        let val = document.getElementById('myvalue').value;
+        if (!val.trim()) return;
 
- }, false);
-}
+        window.location.href =
+            'https://rrrorg9iri.execute-api.eu-west-1.amazonaws.com/default/aya-function?name='
+            + encodeURIComponent(val);
+
+    }, false);
+};
